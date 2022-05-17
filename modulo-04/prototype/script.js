638c43143963a68cbfb4f9c9b4a2da0aa7e24753
Joaoquinto <joaovictoroliveirapereira6522@gmail.com>
2022-05-17 20:28:48 -0300

    Update prototype

--- modulo-04/prototype/script.js
+++ modulo-04/prototype/script.js
@@ -4,10 +4,17 @@ function Pessoa(nome, idade) {
 }
 
 Pessoa.prototype.andar = function () {
-  return 'Pessoa andou'
+  return this.nome + ' Pessoa andou'
 }
+
+
 
 console.log(Pessoa.prototype);
 
 
 const andre = new Pessoa("Andre", 27);
+console.log(andre.andar());
+
+console.log(typeof Pessoa);
+console.log(typeof Pessoa.prototype);
+console.log(typeof andre);
