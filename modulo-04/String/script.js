828e6968a7221af2bad52d6623c2b4fcacbced6a
Joaoquinto <joaovictoroliveirapereira6522@gmail.com>
2022-05-19 22:14:00 -0300

    String 1

--- modulo-04/String/script.js
+++ modulo-04/String/script.js
@@ -33,9 +33,50 @@ if (frutas.includes('Banana', 10)) {
 /* 
  * endsWith & startsWith retorna um boolean se houver match de strings
  * Aceita que informe por onde a procura deve começar, utilizando o index
-*/
+ */
+
+
 if (frutas.endsWith("x")) {
   console.log(frutas.length);
 } else if (frutas.startsWith("M")) {
   console.log("Aye");
 }
+
+const nome = "João Victor O. Pereira"
+
+console.log(nome.length);
+/*  */
+console.log(nome.slice(10, nome.length));
+
+/* 
+ * Retorna o index do primeiro caractere
+ * que der match com o parametro
+ */
+console.log(nome.indexOf("O"));
+
+/* Caso de uso do indexOf & slice */
+const startLastName = nome.indexOf("O")
+const endLastName = nome.length
+const lastName = nome.slice(startLastName, endLastName)
+
+console.log(lastName);
+
+
+
+
+function Loteamento(numeroCasa, QD, ...numerosTelefone) {
+  this.numeroCasa = numeroCasa,
+    this.qD = QD,
+    this.numerosTelefone = numerosTelefone,
+    this.showData = function () {
+      let data = "";
+      data += `Sua casa é N°:${this.numeroCasa} e fica na QD: ${this.qD} e tem os números ${this.numerosTelefone}`
+      return data
+    }
+}
+
+const loteamento = new Loteamento(97, 07, "71 9991792684 ", "71 33289584", "71 33289514", "71 32933337")
+
+
+
+console.log(loteamento.showData());
